45bf49f845377bddcc1444c7ee416e00ca23e874
Bhargav-Thummar <bhargav.thummar810@gmail.com>
2024-03-02 17:32:10 +0530

    Test: Add test case for nil input

--- custom_exceptions.rb
+++ custom_exceptions.rb
@@ -1,1 +1,2 @@
+class NilInputError < StandardError; end
 
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,6 +3,15 @@ require_relative '../string_calculator'
 
 describe StringCalculator do
   context '#add' do
+    let(:string_calculator) { StringCalculator.new }
+
+    context '#NilInputError' do
+      it 'should raise NilInputError for nil' do
+        expect { 
+          string_calculator.add(nil) 
+        }.to raise_error(NilInputError)
+      end
+    end
   end
 end
 
